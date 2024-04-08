1563bd4f5e3ef35b115018083e4cd70d1f13a69b
kajalbk <166373141+kajalbk@users.noreply.github.com>
2024-04-08 13:48:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function myfunction() {
+    document.getElementById('demo2').innerHTML = "change using javascript function.";
+
+}
+
+function displayDate() {
+    document.getElementById("demo5").innerHTML = Date();
+}
